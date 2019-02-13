5c8f038d1a7aec1029dc9a5628a05d68c6b0cd87
Roman Shishkin <spark@uwtech.org>
2019-02-13 15:55:28 +0300

    Terminator bot wip

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from andrew.api.plugin import AbstractPlugin
 
 
@@ -7,8 +9,11 @@ class Plugin(AbstractPlugin):
 
         self.set_settings({
             'message': 'Привет, {}!',
-            'terminator': False
+            'terminator': False,
+            'terminator_msg': 'У нас тут включена защита. У тебя есть минута, чтобы пройти проверку.'
         })
+
+        self.terminator_cache = {}
 
     def post_connect(self):
         connections = self.andrew.connections.connections
@@ -24,3 +29,7 @@ class Plugin(AbstractPlugin):
         nickname += ' {}'.format(message['last_name']) if 'last_name' in message else ''
         msg = self.get_settings(chat.id).get('message')
         await chat.send_text(msg.format(nickname))
+
+        if self.get_settings(chat.id).get('terminator'):
+            terminator_msg = await chat.send_text(self.get_settings(chat.id).get('terminator_msg'))
+            #await asyncio.sleep(60)
